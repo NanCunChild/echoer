2524bc7b13fd5c3e7531f5a6c36533580df65517
NanCunChild <nancunchild@gmail.com>
2023-10-14 00:55:44 +0800

    A little bit strange bug...

--- chat_board/src/main/java/com/example/chat_board/ActivityChat.java
+++ chat_board/src/main/java/com/example/chat_board/ActivityChat.java
@@ -9,6 +9,6 @@ public class ActivityChat extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat_ui);
+        setContentView(R.layout.activity_chat);
     }
 }
