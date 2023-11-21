fa4cc3f636554f7492eb30593bc2c13a3189f994
Riosky <duodecimn@qq.com>
2023-11-21 21:12:38 +0800

    to: tried to fix the problem of RecyclerView not showing

--- app/src/main/java/com/example/echoer/activities/ChatActivity.java
+++ app/src/main/java/com/example/echoer/activities/ChatActivity.java
@@ -6,6 +6,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.echoer.databinding.ActivityChatBinding;
 import com.example.echoer.models.ChatMessage;
@@ -24,14 +27,17 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityChatBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        View view = binding.getRoot();
+        setContentView(view);
+
         setListeners();
-        initMessageList();
         init();
 
-        Intent intent = getIntent();
-        String deviceName = intent.getStringExtra("DEVICE_NAME");
-        String deviceAddress = intent.getStringExtra("DEVICE_ADDRESS");
+        binding.inputText.setText("让我看看！！");
+
+//        Intent intent = getIntent();
+//        String deviceName = intent.getStringExtra("DEVICE_NAME");
+//        String deviceAddress = intent.getStringExtra("DEVICE_ADDRESS");
         // 使用这些数据进行相关操作
     }
 
@@ -40,14 +46,18 @@ public class ChatActivity extends AppCompatActivity {
         LinearLayoutManager manager = new LinearLayoutManager(this);
         manager.setOrientation(RecyclerView.VERTICAL);
         binding.chatRecycleView.setLayoutManager(manager);
+//        binding.chatRecycleView.smoothScrollToPosition(0);
 
-        chatMessages = new ArrayList<>();
-        chatAdapter = new ChatMessageAdapter(chatMessages);
+        //初始化聊天界面
+        initMessageList();
+        Log.d("In init()", "第一条短信为" + chatMessages.get(0).getMessage());
+//        binding.chatRecycleView.notify();
+        chatAdapter = new ChatMessageAdapter(chatMessages, this);
         binding.chatRecycleView.setAdapter(chatAdapter);
     }
 
     private void initMessageList() {
-        chatMessages = new ArrayList<ChatMessage>();
+        chatMessages = new ArrayList<>();
         ChatMessage msg1 = new ChatMessage("20231022", "Hello, World!", "Jobs", Constants.VIEW_TYPE_RECEIVED);
         chatMessages.add(msg1);
         ChatMessage msg2 = new ChatMessage("20231026", "Beep", "Me", Constants.VIEW_TYPE_SENT);
@@ -64,7 +74,9 @@ public class ChatActivity extends AppCompatActivity {
             // 将消息添加到消息列表里
             if (!message.equals("")) {
                 chatMessages.add(new ChatMessage("20231029", message, "Me", Constants.VIEW_TYPE_SENT));
+                chatAdapter.notifyDataSetChanged(); // 通知适配器数据发生了变化
             }
+            Toast.makeText(this, "I am so desperate.", Toast.LENGTH_SHORT).show();
         });
     }
 }
--- app/src/main/java/com/example/echoer/adapters/ChatMessageAdapter.java
+++ app/src/main/java/com/example/echoer/adapters/ChatMessageAdapter.java
@@ -1,12 +1,16 @@
 package com.example.echoer.adapters;
 
+import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.echoer.R;
 import com.example.echoer.databinding.ItemContainerReceivedMessageBinding;
 import com.example.echoer.databinding.ItemContainerSentMessageBinding;
 import com.example.echoer.models.ChatMessage;
@@ -15,18 +19,18 @@ import com.example.echoer.utilities.Constants;
 import java.util.List;
 
 public class ChatMessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-    // 信息列表
     private final List<ChatMessage> chatMessageList;
 
-    public ChatMessageAdapter(List<ChatMessage> chatMessageList) {
+    public ChatMessageAdapter(List<ChatMessage> chatMessageList, Context context) {
         this.chatMessageList = chatMessageList;
     }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        Log.d("tag", "why!!!!!!");
-        if (viewType == Constants.VIEW_TYPE_SENT) {   // 处理由“我”发出来的信息
+//        Log.d("tag", "why!!!!!!");
+
+        if (viewType == Constants.VIEW_TYPE_SENT) {
             return new SentMassageViewHolder(
                     ItemContainerSentMessageBinding.inflate(
                             LayoutInflater.from(parent.getContext()),
@@ -43,15 +47,26 @@ public class ChatMessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
                     )
             );
         }
+
+//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_container_sent_message, parent, false);
+//        return new testViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         if (getItemViewType(position) == Constants.VIEW_TYPE_SENT) {
-            Log.d("adapter", (chatMessageList.get(position)).getMessage());
             ((SentMassageViewHolder) holder).setData((chatMessageList.get(position)));
         } else {
             ((ReceivedMassageViewHolder) holder).setData(chatMessageList.get(position));
+        }
+//        ((testViewHolder) holder).test.setText(chatMessageList.get(position).getMessage());
+    }
+
+    static class testViewHolder extends RecyclerView.ViewHolder {
+        TextView test;
+        public testViewHolder(@NonNull View itemView) {
+            super(itemView);
+            test = itemView.findViewById(R.id.textMessage);
         }
     }
 
@@ -65,7 +80,6 @@ public class ChatMessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
         return chatMessageList.get(position).getIsSent();
     }
 
-    // 装载发送消息的视图
     static class SentMassageViewHolder extends RecyclerView.ViewHolder {
 
         private final ItemContainerSentMessageBinding binding;
@@ -81,7 +95,6 @@ public class ChatMessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
         }
     }
 
-    // 装载收到消息的视图
     static class ReceivedMassageViewHolder extends RecyclerView.ViewHolder {
 
         private final ItemContainerReceivedMessageBinding binding;
