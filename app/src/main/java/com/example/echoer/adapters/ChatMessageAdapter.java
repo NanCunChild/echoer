7ffd015931fb3a71c8789c5c1c3f582e99e3aaa9
Riosky <duodecimn@qq.com>
2023-12-05 00:07:59 +0800

    more

--- app/src/main/java/com/example/echoer/adapters/ChatMessageAdapter.java
+++ app/src/main/java/com/example/echoer/adapters/ChatMessageAdapter.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+//import com.bumptech.glide.Glide;
 import com.example.echoer.databinding.ItemContainerReceivedMessageBinding;
 import com.example.echoer.databinding.ItemContainerSentMessageBinding;
 import com.example.echoer.models.ChatMessage;
@@ -47,13 +48,24 @@ public class ChatMessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        ChatMessage chatMessage = chatMessageList.get(position);
         if (getItemViewType(position) == Constants.VIEW_TYPE_SENT) {
-            Log.d("adapter", (chatMessageList.get(position)).getMessage());
-            ((SentMessageViewHolder) holder).setData((chatMessageList.get(position)));
+            ((SentMessageViewHolder) holder).setData(chatMessage);
+            // 如果消息包含图片，设置ImageView可见并加载图片
+//            if (chatMessage.getImageUrl() != null && !chatMessage.getImageUrl().isEmpty()) {
+//                ((SentMassageViewHolder) holder).binding.imageMessage.setVisibility(View.VISIBLE);
+//                // 使用Glide或其他库加载图片
+//                // Glide.with(holder.itemView.getContext()).load(chatMessage.getImageUrl()).into(((SentMassageViewHolder) holder).binding.imageView);
+//            } else {
+//                ((SentMassageViewHolder) holder).binding.imageMessage.setVisibility(View.GONE);
+//            }
         } else {
-            ((ReceivedMessageViewHolder) holder).setData(chatMessageList.get(position));
+            ((ReceivedMessageViewHolder) holder).setData(chatMessage);
+            // 同样的逻辑适用于接收的消息
+            // ...
         }
     }
+
 
     @Override
     public int getItemCount() {
@@ -67,19 +79,28 @@ public class ChatMessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
 
     // 装载发送消息的视图
     static class SentMessageViewHolder extends RecyclerView.ViewHolder {
-
         private final ItemContainerSentMessageBinding binding;
 
-        SentMessageViewHolder(ItemContainerSentMessageBinding itemContainerSentMessageBinding) {
-            super(itemContainerSentMessageBinding.getRoot());
-            binding = itemContainerSentMessageBinding;
+        SentMessageViewHolder(ItemContainerSentMessageBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
         }
 
         void setData(ChatMessage chatMessage) {
             binding.textDateTime.setText(chatMessage.getDateTime());
             binding.textMessage.setText(chatMessage.getMessage());
+
+//            if (chatMessage.getImageUrl() != null && !chatMessage.getImageUrl().isEmpty()) {
+//                binding.imageMessage.setVisibility(View.VISIBLE);
+//                Glide.with(binding.imageMessage.getContext())
+//                        .load(chatMessage.getImageUrl())
+//                        .into(binding.imageMessage);
+//            } else {
+//                binding.imageMessage.setVisibility(View.GONE);
+//            }
         }
     }
+
 
     // 装载收到消息的视图
     static class ReceivedMessageViewHolder extends RecyclerView.ViewHolder {
